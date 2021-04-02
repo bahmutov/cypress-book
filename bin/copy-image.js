b3e7b488adb17638b8943694230980a847c92832
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-02 17:31:12 -0400

    implement copying

--- bin/copy-image.js
+++ bin/copy-image.js
@@ -4,6 +4,7 @@
 
 const debug = require('debug')('cypress-book')
 const arg = require('arg')
+
 const args = arg({
   '--screenshot': String,
   '--output': String,
@@ -14,3 +15,13 @@ const args = arg({
 })
 
 debug('input arguments %o', args)
+
+const { copyImage } = require('../src/copy-image')
+
+copyImage({
+  inputImagePath: args['--screenshot'],
+  outputFilename: args['--output'],
+}).catch((e) => {
+  console.error(e)
+  process.exit(1)
+})
